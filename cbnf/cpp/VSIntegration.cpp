14103c289c528d27209631003e39407446513908
Semi Essessi <semi@cranium-software.co.uk>
2017-12-15 18:38:02 +0000

    project type vsix project generation

--- cbnf/cpp/VSIntegration.cpp
+++ cbnf/cpp/VSIntegration.cpp
@@ -2,6 +2,8 @@
 
 #include "VSIntegration.h"
 
+#include "VSProjectTypeData.h"
+
 #include "../../common/cpp/FileSystem.h"
 #include "../../parser/cpp/Grammar.h"
 
@@ -99,6 +101,40 @@ static void WriteSolution( const char* const szPath, const char* const szName )
 	WriteTextFile( szFinalPath.c_str(), szOutput.c_str() );
 }
 
+static void WriteCSProjectType(
+	const char* const szPath,
+	const char* const szName,
+	const char* const szGUID )
+{
+	std::string szOutput = kaszVSProjectData[ 0 ];
+	szOutput += szGUID;
+	szOutput += kaszVSProjectData[ 1 ];
+	for( size_t i = 2; i < sizeof( kaszVSProjectData ) / sizeof( kaszVSProjectData[ 0 ] ); ++i )
+	{
+		szOutput += szName;
+		szOutput += kaszVSProjectData[ i ];
+	}
+
+	std::string szFinalPath( szPath );
+	szFinalPath += "/";
+	szFinalPath += szName;
+	szFinalPath += ".ProjectType.csproj";
+	WriteTextFile( szFinalPath.c_str(), szOutput.c_str() );
+}
+
+static void WriteCSharpProjects( const char* const szPath, const char* const szName )
+{
+	// write project template
+	//WriteCSProjectTemplate(
+	//	( std::string( szPath ) + "/ProjectTemplate" ).c_str(),
+	//	szName, GetProjectGuid( 0 ).c_str() );
+
+	// write project type
+	WriteCSProjectType(
+		( std::string( szPath ) + "/ProjectType" ).c_str(),
+		szName, GetProjectGuid( 1 ).c_str() );
+}
+
 void CreateIntegrationInPath(
 	const char* const szPath,
 	const Parser::Grammar& xGrammar,
@@ -109,8 +145,7 @@ void CreateIntegrationInPath(
 
 	WriteSolution( szNewPath.c_str(), xGrammar.GetName().c_str() );
 
-	//WriteCSharpProjects( szPath, szName ); // ...
-
+	WriteCSharpProjects( szNewPath.c_str(), xGrammar.GetName().c_str() );
 
 }
 
