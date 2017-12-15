9d33dcb73c51badb2c5bf95232e40f594035dcd1
Semi Essessi <semi@cranium-software.co.uk>
2017-12-15 22:59:38 +0000

    even more project type content

--- cbnf/cpp/VSIntegration.cpp
+++ cbnf/cpp/VSIntegration.cpp
@@ -5,6 +5,7 @@
 #include "DefaultVSIconData.h"
 #include "VSProjectTemplateData.h"
 #include "VSProjectTypeData.h"
+#include "VSProjectTypeRulesData.h"
 
 #include "../../common/cpp/FileSystem.h"
 #include "../../parser/cpp/Grammar.h"
@@ -241,26 +242,68 @@ static void WriteTemplateFiles( const char* const szPath, const Parser::Grammar&
 
 static void WriteProjectTypeFiles( const char* const szPath, const Parser::Grammar& xGrammar )
 {
+	EnsurePath( ( std::string( szPath ) + "/BuildSystem" ).c_str() );
+	EnsurePath( ( std::string( szPath ) + "/Properties" ).c_str() );
+	EnsurePath( ( std::string( szPath ) + "/BuildSystem/DeployedBuildSystem" ).c_str() );
+	EnsurePath( ( std::string( szPath ) + "/BuildSystem/Rules" ).c_str() );
+
 	std::string szFinalPath( szPath );
 	szFinalPath += "/packages.config";
 
 	// packages config
 	WriteTextFile( szFinalPath.c_str(),
-		"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
-		"<packages>\n"
-		"  <package id=\"Microsoft.Composition\" version=\"1.0.30\" targetFramework=\"net46\" />\n"
-		"  <package id=\"Microsoft.Tpl.Dataflow\" version=\"4.5.24\" targetFramework=\"net46\" />\n"
-		"  <package id=\"Microsoft.VisualStudio.Composition\" version=\"15.0.71\" targetFramework=\"net46\" />\n"
-		"  <package id=\"Microsoft.VisualStudio.ProjectSystem\" version=\"15.0.743\" targetFramework=\"net46\" />\n"
-		"  <package id=\"Microsoft.VisualStudio.ProjectSystem.Analyzers\" version=\"15.0.743\" targetFramework=\"net46\" developmentDependency=\"true\" />\n"
-		"  <package id=\"Microsoft.VisualStudio.ProjectSystem.SDK\" version=\"15.0.743\" targetFramework=\"net46\" />\n"
-		"  <package id=\"Microsoft.VisualStudio.ProjectSystem.SDK.Tools\" version=\"15.0.743\" targetFramework=\"net46\" developmentDependency=\"true\" />\n"
-		"  <package id=\"Microsoft.VisualStudio.SDK.VsixSuppression\" version=\"14.1.32\" targetFramework=\"net46\" />\n"
-		"  <package id=\"Microsoft.VisualStudio.Threading\" version=\"15.0.240\" targetFramework=\"net46\" />\n"
-		"  <package id=\"Microsoft.VisualStudio.Threading.Analyzers\" version=\"15.0.240\" targetFramework=\"net46\" developmentDependency=\"true\" />\n"
-		"  <package id=\"Microsoft.VisualStudio.Validation\" version=\"15.0.82\" targetFramework=\"net46\" />\n"
-		"  <package id=\"System.Collections.Immutable\" version=\"1.3.1\" targetFramework=\"net46\" />\n"
+		"<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n"
+		"<packages>\r\n"
+		"  <package id=\"Microsoft.Composition\" version=\"1.0.30\" targetFramework=\"net46\" />\r\n"
+		"  <package id=\"Microsoft.Tpl.Dataflow\" version=\"4.5.24\" targetFramework=\"net46\" />\r\n"
+		"  <package id=\"Microsoft.VisualStudio.Composition\" version=\"15.0.71\" targetFramework=\"net46\" />\r\n"
+		"  <package id=\"Microsoft.VisualStudio.ProjectSystem\" version=\"15.0.743\" targetFramework=\"net46\" />\r\n"
+		"  <package id=\"Microsoft.VisualStudio.ProjectSystem.Analyzers\" version=\"15.0.743\" targetFramework=\"net46\" developmentDependency=\"true\" />\r\n"
+		"  <package id=\"Microsoft.VisualStudio.ProjectSystem.SDK\" version=\"15.0.743\" targetFramework=\"net46\" />\r\n"
+		"  <package id=\"Microsoft.VisualStudio.ProjectSystem.SDK.Tools\" version=\"15.0.743\" targetFramework=\"net46\" developmentDependency=\"true\" />\r\n"
+		"  <package id=\"Microsoft.VisualStudio.SDK.VsixSuppression\" version=\"14.1.32\" targetFramework=\"net46\" />\r\n"
+		"  <package id=\"Microsoft.VisualStudio.Threading\" version=\"15.0.240\" targetFramework=\"net46\" />\r\n"
+		"  <package id=\"Microsoft.VisualStudio.Threading.Analyzers\" version=\"15.0.240\" targetFramework=\"net46\" developmentDependency=\"true\" />\r\n"
+		"  <package id=\"Microsoft.VisualStudio.Validation\" version=\"15.0.82\" targetFramework=\"net46\" />\r\n"
+		"  <package id=\"System.Collections.Immutable\" version=\"1.3.1\" targetFramework=\"net46\" />\r\n"
 		"</packages>" );
+
+	szFinalPath = szPath;
+	szFinalPath += "/app.config";
+
+	// app config
+	WriteTextFile( szFinalPath.c_str(),
+		"<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n"
+		"<configuration>\r\n"
+		"  <runtime>\r\n"
+		"    <assemblyBinding xmlns=\"urn:schemas-microsoft-com:asm.v1\">\r\n"
+		"      <dependentAssembly>\r\n"
+		"        <assemblyIdentity name=\"EnvDTE\" publicKeyToken=\"b03f5f7f11d50a3a\" culture=\"neutral\" />\r\n"
+		"        <bindingRedirect oldVersion=\"0.0.0.0-8.0.0.0\" newVersion=\"8.0.0.0\" />\r\n"
+		"      </dependentAssembly>\r\n"
+		"      <dependentAssembly>\r\n"
+		"        <assemblyIdentity name=\"System.Collections.Immutable\" publicKeyToken=\"b03f5f7f11d50a3a\" culture=\"neutral\" />\r\n"
+		"        <bindingRedirect oldVersion=\"0.0.0.0-1.3.1.0\" newVersion=\"1.3.1.0\" />\r\n"
+		"      </dependentAssembly>\r\n"
+		"      <dependentAssembly>\r\n"
+		"        <assemblyIdentity name=\"Microsoft.Build.Utilities.Core\" publicKeyToken=\"b03f5f7f11d50a3a\" culture=\"neutral\" />\r\n"
+		"        <bindingRedirect oldVersion=\"0.0.0.0-15.1.0.0\" newVersion=\"15.1.0.0\" />\r\n"
+		"      </dependentAssembly>\r\n"
+		"      <dependentAssembly>\r\n"
+		"        <assemblyIdentity name=\"Microsoft.Build.Framework\" publicKeyToken=\"b03f5f7f11d50a3a\" culture=\"neutral\" />\r\n"
+		"        <bindingRedirect oldVersion=\"0.0.0.0-15.1.0.0\" newVersion=\"15.1.0.0\" />\r\n"
+		"      </dependentAssembly>\r\n"
+		"      <dependentAssembly>\r\n"
+		"        <assemblyIdentity name=\"System.Composition.AttributedModel\" publicKeyToken=\"b03f5f7f11d50a3a\" culture=\"neutral\" />\r\n"
+		"        <bindingRedirect oldVersion=\"0.0.0.0-1.0.30.0\" newVersion=\"1.0.30.0\" />\r\n"
+		"      </dependentAssembly>\r\n"
+		"      <dependentAssembly>\r\n"
+		"        <assemblyIdentity name=\"System.Composition.Runtime\" publicKeyToken=\"b03f5f7f11d50a3a\" culture=\"neutral\" />\r\n"
+		"        <bindingRedirect oldVersion=\"0.0.0.0-1.0.30.0\" newVersion=\"1.0.30.0\" />\r\n"
+		"      </dependentAssembly>\r\n"
+		"    </assemblyBinding>\r\n"
+		"  </runtime>\r\n"
+		"</configuration>" );
 
 	szFinalPath = szPath;
 	szFinalPath += "/ProjectProperties.cs";
@@ -296,6 +339,76 @@ static void WriteProjectTypeFiles( const char* const szPath, const Parser::Gramm
 	// another project class
 	WriteTextFile( szFinalPath.c_str(),
 		( ( std::string( "namespace " ) + xGrammar.GetName() + "\r\n" ) + szOutput ).c_str() );
+
+	// some package class thingy
+	szFinalPath = szPath;
+	szFinalPath += "/VSPackage.cs";
+
+	szOutput = kaszVSPackageCSData[ 0 ];
+	szOutput += "package-guid"; // SE - TODO: ...
+	szOutput += kaszVSPackageCSData[ 1 ];
+	szOutput += "project-type-guid"; // SE - TODO: ...
+	szOutput += kaszVSPackageCSData[ 2 ];
+	szOutput += xGrammar.GetShortName();
+	szOutput += kaszVSPackageCSData[ 3 ];
+	szOutput += xGrammar.GetName();
+	szOutput += kaszVSPackageCSData[ 4 ];
+
+	WriteTextFile( szFinalPath.c_str(), szOutput.c_str() );
+
+	// resource files ...
+	szFinalPath = szPath;
+	szFinalPath += "/Resources.resx";
+
+	WriteTextFile( szFinalPath.c_str(), kszResourcesResX );
+
+	szFinalPath = szPath;
+	szFinalPath += "/VSPackage.resx";
+
+	szOutput = kaszVSPackageResXData[ 0 ];
+	for ( size_t i = 1; i < sizeof( kaszVSPackageResXData ) / sizeof( kaszVSPackageResXData[ 0 ] ); ++i )
+	{
+		if( ( i == 3 ) || ( i == 4 ) )
+		{
+			szOutput += xGrammar.GetShortName();
+		}
+		else
+		{
+			szOutput += xGrammar.GetName();
+		}
+
+		szOutput += kaszVSPackageResXData[ i ];
+	}
+
+	WriteTextFile( szFinalPath.c_str(), szOutput.c_str() );
+
+	// some manifest...
+
+	szFinalPath = szPath;
+	szFinalPath += "/source.extension.vsixmanifest";
+
+	szOutput = kaszVSPackageManifestData[ 0 ];
+	for ( size_t i = 1; i < sizeof( kaszVSPackageManifestData ) / sizeof( kaszVSPackageManifestData[ 0 ] ); ++i )
+	{
+		szOutput += xGrammar.GetName();
+		szOutput += kaszVSPackageManifestData[ i ];
+	}
+
+	WriteTextFile( szFinalPath.c_str(), szOutput.c_str() );
+
+	// some assembly info cs
+
+	szFinalPath = szPath;
+	szFinalPath += "/Properties/AssemblyInfo.cs";
+
+	szOutput = kaszVSAssemblyInfoCSData[ 0 ];
+	for ( size_t i = 1; i < sizeof( kaszVSAssemblyInfoCSData ) / sizeof( kaszVSAssemblyInfoCSData[ 0 ] ); ++i )
+	{
+		szOutput += xGrammar.GetName();
+		szOutput += kaszVSAssemblyInfoCSData[ i ];
+	}
+
+	WriteTextFile( szFinalPath.c_str(), szOutput.c_str() );
 }
 
 void CreateIntegrationInPath(
---
+++ cbnf/cpp/VSProjectTypeRulesData.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2017 Cranium Software
+
+#ifndef VS_PROJECT_TYPE_RULES_DATA_H
+#define VS_PROJECT_TYPE_RULES_DATA_H
+
+namespace CP2
+{
+
+}
+
+#endif
